47f5a8094552354a373721f4ec506a5c62e6941e
ajbelchior <celchior@gmail.com>
2026-06-01 21:56:39 -0300

    exercicio003

---
+++ exe003.py
@@ -0,0 +1,7 @@
+# Crie um programa que leia dois números e mostre a soma entre eles.
+
+n1 = int(input("Informe o primeiro valor: "))
+n2 = int(input("Informe o segundo valor: "))
+s = (n1 + n2)
+
+print(f"A soma entre {n1} e {n2} = {s}")
